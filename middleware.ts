852c634630c551feb79fd14bc645ac9745ceb822
Muhammad Saifullah <118552140+Muhammad-Saifullah50@users.noreply.github.com>
2024-01-06 12:49:13 +0500

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,13 +6,16 @@ export const middleware = async (request: NextRequest) => {
     
     let token = request.cookies.get('next-auth.session-token') || request.cookies.get('__Secure-next-auth.session-token')
 
+    // specify your public routes
     const publicPath = pathname === '/' || pathname === '/login' || pathname === '/register' || pathname === '/search' || pathname.includes('/listings')
 
+    // redirect to your login 
     if (!publicPath && !token) {
         const response = NextResponse.redirect(new URL('/login', request.nextUrl))
         return response
 
     }
+    // adjust acording to your app
     if (token && pathname === '/signin') {
         const response = NextResponse.redirect(new URL('/', request.nextUrl))
         return response
@@ -24,7 +27,7 @@ export const middleware = async (request: NextRequest) => {
 }
 
 
-
+// adjust according to you routes
 export const config =
 {
     matcher: [
